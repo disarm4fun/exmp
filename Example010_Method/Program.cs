c3b665549fb9e25ed3a343a7d1835b1374194c96
joyez <joyezz333@gmail.com>
2022-12-02 11:50:00 +0300

    method1

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 12, 22, 33, 44, 55, 66, 44, 77, 98 };
+
+int n = array.Length;
+int find = 44;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
